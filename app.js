ad734a61d9e2b8aee49c00161634a5866cc81eac
Gokul NB <gokulbabu9278@gmail.com>
2024-07-04 16:04:25 +0530

    admin and user panel created additional product add section includes

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
+
 var hbs = require('express-handlebars');
+var fileUpload = require('express-fileupload');
 
 var app = express();
 
@@ -20,8 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(fileUpload());
+
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
