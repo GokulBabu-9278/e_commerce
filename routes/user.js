8677a0d57057dd56e77513fd1c2a65df000e2efe
Gokul NB <gokulbabu9278@gmail.com>
2024-07-16 10:08:28 +0530

    some errors resolved, signup login form created and success full get data using post method

--- routes/user.js
+++ routes/user.js
@@ -8,8 +8,19 @@ router.get('/', function(req, res, next) {
 
   productHelper.getAllProducts().then((products)=>{  // promise reason for then
     //console.log(products)
-    res.render('user/user_view_products', {admin:true, products})
+    res.render('user/user_view_products', {admin:false, products})
   })
 });
+router.get('/login', (req,res)=>{
+  res.render('user/login')
+})
+router.get('/signup', (req,res)=>{
+  res.render('user/signup')
+})
+router.post('/signup', (req,res)=>{
+  
+  console.log(req.body)
+  res.render('user/login')
+})
 
 module.exports = router;
